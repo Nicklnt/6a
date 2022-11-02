603763d16c5f3f5e18e700b83a071fdc7449d315
Nicklnt <116169364+Nicklnt@users.noreply.github.com>
2022-11-02 17:23:06 +0100

    assigment classes
    add one more funktion in the class and call it from main

--- myClass.cpp
+++ myClass.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
 using namespace std;
 
-class myClass {
-            char name[25];
-            int age;
-        public:
+class myClass
+{
+    char name[25];
+    int age;
 
-            void showInfo()
-            {
-                cout << "Hi my name is " << this->name << " and Im " << this->age << " old." << endl;
-            }
-
+public:
+    void showInfo()
+    {
+        cout << "Hi my name is " << this->name << " and Im " << this->age << " old." << endl;
+    }
+    void getInfo()
+    {
+        cout << "What is my name?" << endl;
+        cin >> name;
+        cout << "What is my age?" << endl;
+        cin >> age;
+    }
+    
 };
 
-int main(int argc,char **argv)
+int main(int argc, char **argv)
 {
-        myClass me;
+    myClass me;
 
-        cout << "What is my name?" << endl;
-        cin >> me.name;
-        cout << "What is my age?" << endl;
-        cin >> me.age;
+    me.getInfo();
+    me.showInfo();
+   
 
-        me.showInfo();
-
-        return 0;
+    return 0;
 }
